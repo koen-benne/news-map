3c467dcaec4ecfc422e8bd703e2838e2d5e994e4
koen-benne <koen.benne@gmail.com>
2020-11-03 16:19:25 +0100

    Added map to home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { environment } from '../../environments/environment';
+
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
-  constructor() {}
+export class HomePage implements OnInit {
+  map: mapboxgl.Map;
+  style = 'mapbox://styles/mapbox/streets-v11';
+
+  lat = 52;
+  lng = 5.5;
+  constructor() { }
+
+  ngOnInit() {
+    setTimeout(() => this.buildMap(), 0);
+  }
+
+  private buildMap() {
+    this.map = new mapboxgl.Map({
+      accessToken: environment.mapbox.accessToken,
+      container: 'map',
+      style: this.style,
+      zoom: 13,
+      center: [this.lng, this.lat],
+      bounds: [[3, 50.2], [7.6, 53.8]],
+    });
+
+    this.map.setMaxBounds(this.map.getBounds());
+  }
 
 }
+
