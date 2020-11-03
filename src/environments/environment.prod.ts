2e4cfd7ac248063a8253c5d7fc58ed9b4bdbaa48
koen-benne <koen.benne@gmail.com>
2020-11-03 15:52:01 +0100

    Added mapbox acces token

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoieWVyYm95Y29uZSIsImEiOiJja2dqdDdmYmwweXMwMnFxbjU5dGY4d3pmIn0.C_IhnFvdZ1huOB7D4d77NQ'
+  }
 };
