fb9d4b1bd83a1062e47f3ed811c34127b4d47610
koen-benne <koen.benne@gmail.com>
2020-12-02 16:53:11 +0100

    Styled categories and made them semi-functional

--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -3,6 +3,7 @@ import { transition } from '../animations/news';
 import { Component, OnInit} from '@angular/core';
 import { environment } from '../../environments/environment';
 import * as mapboxgl from 'mapbox-gl';
+import {Platform} from '@ionic/angular';
 
 @Component({
   selector: 'app-map',
@@ -18,7 +19,16 @@ export class MapPage implements OnInit {
 
   lat = 52;
   lng = 5.5;
-  constructor(private http: HttpClient) { }
+
+  categories = [
+      new Category(0, 'Sport', ['s1', 's2', 's3']),
+      new Category(1, 'Cultuur', ['c1', 'c2', 'c3']),
+      new Category(2, '112', ['11', '12', '13']),
+      new Category(3, 'Politiek', ['p1', 'p2', 'p3']),
+  ];
+  selectedCategory = this.categories[0];
+
+  constructor(private http: HttpClient, public platform: Platform) { }
 
   ngOnInit() {
     // For some reason the map takes the correct size when its put in the event loop like this...
@@ -41,5 +51,27 @@ export class MapPage implements OnInit {
     this.map.setMaxBounds(this.map.getBounds());
     this.map.setZoom(6.5);
   }
+
+  private selectCategory(id: number) {
+    if (this.selectedCategory.id !== id) {
+      for (const category of this.categories) {
+        if (category.id === id) {
+          this.selectedCategory = category;
+          return;
+        }
+      }
+    }
+  }
 }
 
+export class Category {
+  id: number;
+  name: string;
+  filters: string[];
+
+  constructor(id: number, name: string, filters: string[]) {
+    this.id = id;
+    this.name = name;
+    this.filters = filters;
+  }
+}
