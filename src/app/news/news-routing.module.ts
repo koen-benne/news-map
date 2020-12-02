d4ff9ecbec9edad56d717c094df91ae0ab526f8a
koen-benne <koen.benne@gmail.com>
2020-12-02 10:44:41 +0100

    Renamed news to article and created new news page

--- src/app/news/news-routing.module.ts
+++ src/app/news/news-routing.module.ts
@@ -2,11 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { NewsPage } from './news.page';
+import {MapPage} from '../map/map.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: NewsPage
+    children: [
+      {
+        path: '',
+        component: NewsPage,
+      },
+      {
+        path: 'article',
+        loadChildren: () => import('../article/article.module').then(m => m.ArticlePageModule)
+      },
+    ]
   }
 ];
 
