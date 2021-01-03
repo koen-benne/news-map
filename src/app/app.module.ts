2276a4864d45dd2a17ae9ab6e26cf6c4f49a219e
TheSas99 <thesas99games@gmail.com>
2021-01-03 13:16:07 +0100

    Added Login System with registration and login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouteReuseStrategy} from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
+import {SplashScreen} from '@ionic-native/splash-screen/ngx';
+import {StatusBar} from '@ionic-native/status-bar/ngx';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
 import {HttpClientModule} from '@angular/common/http';
+
+import {AngularFireModule} from '@angular/fire';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import {environment} from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot({
+  imports: [
+      BrowserModule, IonicModule.forRoot({
 
-  }), AppRoutingModule, HttpClientModule],
+  }), AppRoutingModule, HttpClientModule, AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebase)],
   providers: [
     StatusBar,
     SplashScreen,
